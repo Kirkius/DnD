324bd251277ee883a11344479ae3266640b126aa
Kirkius <casper.kerk@gmail.com>
2020-06-04 16:20:05 +0200

    Added POST body

--- src/main/java/nl/YCDP2/ITFund/view/TafelEndpoint.java
+++ src/main/java/nl/YCDP2/ITFund/view/TafelEndpoint.java
@@ -2,9 +2,13 @@ package nl.YCDP2.ITFund.view;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import nl.YCDP2.ITFund.controller.TafelService;
+import nl.YCDP2.ITFund.model.Tafel;
 
 @RestController
 public class TafelEndpoint {
@@ -12,9 +16,17 @@ public class TafelEndpoint {
     @Autowired
     TafelService tafelService;
 
-    @GetMapping("/tafel")
-    public void testFunctie(){
+    @GetMapping("/tafel/{ap}")
+    public Tafel testFunctie(@PathVariable String ap){
         System.out.println("Hij doet het! :D");
         tafelService.getTafel();
+        return new Tafel();
+    }
+    @PostMapping("/nwtafel")
+    public String test(@RequestBody Tafel tafel){
+        System.out.println("ff post checken");
+        System.out.println(">>"+ tafel.getAantalPersonen());
+        System.out.println(">>>"+ tafel.getTafelnummer());
+        return ("return van post");
     }
 }
