4805c640ff791ead4d63af9d45049c2a70d69ab7
Kirkius <casper.kerk@gmail.com>
2020-06-04 11:34:51 +0200

    Eerste master

---
+++ src/main/java/nl/YCDP2/ITFund/ItFundApplication.java
@@ -0,0 +1,13 @@
+package nl.YCDP2.ITFund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItFundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItFundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/YCDP2/ITFund/ItFundApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.YCDP2.ITFund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItFundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
